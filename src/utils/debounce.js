29165b314f87c42ef4445778a89c9f9556418315
Vadim Kochergin <tamato2008@rambler.ru>
2020-03-20 21:21:57 +0300

    debounce util

---
+++ src/utils/debounce.js
@@ -0,0 +1,9 @@
+export default function debounce (callback, timeout = 0) {
+   let timer
+   return function () {
+       clearTimeout(timer)
+       timer = setTimeout(() => {
+         callback.apply(this, arguments)
+       }, timeout)
+   }
+}
